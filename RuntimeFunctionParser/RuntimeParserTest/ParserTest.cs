8588d8ce4e101898f913f9985f5501e6c6450af8
Christian Bechtluft <development@fam-bechtluft.de>
2017-04-14 17:55:27 +0200

    Added UnitTestProject
    Added a UnitTest Project with few tests for the Parser

---
+++ RuntimeFunctionParser/RuntimeParserTest/ParserTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RuntimeFunctionParser;
+
+namespace RuntimeParserTest
+{
+	[TestClass]
+	public class ParserTest
+	{
+		private static Parser _parser;
+
+		[ClassInitialize]
+		public static void InitializeClass(TestContext tc)
+		{
+			_parser = new Parser();
+		}
+
+		[TestMethod]
+		public void Power_Test()
+		{
+			string function = "2*x^2";
+
+			Function f = _parser.ParseFunction(function);
+			
+			Assert.IsNotNull(f);
+		}
+
+		[TestMethod]
+		public void PolynominalParse_Test()
+		{
+			string function = "2+-1,27411634756996*x+0*x^2+2,27411634756996*x^3";
+
+			Function f = _parser.ParseFunction(function);
+			Assert.IsNotNull(f);
+		}
+	}
+}
